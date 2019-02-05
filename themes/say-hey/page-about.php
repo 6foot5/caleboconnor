5c0925985b0dfe6b2c42722ecfff42001dd2fc5e
6foot5 <rbarley@gmail.com>
2019-02-04 21:14:11 -0500

    added some styles to about page

--- themes/say-hey/page-about.php
+++ themes/say-hey/page-about.php
@@ -18,6 +18,20 @@ get_header();
 	<div id="primary" class="content-area content-area--padded-sides">
 		<main id="main" class="site-main">
 
+			<div class="flexible">
+				<div class="flexible__flex-left">
+					<img class="about-photo" alt="Caleb O'Connor" src="<?php the_post_thumbnail_url('gallery-category'); ?>">
+				</div>
+				<div class="flexible__flex-right">
+					<h2 class="about-head">About Caleb</h2>
+					<hr class="about-hr" />
+					<a href="#" class="about-link">Resume</a>
+					<a href="#" class="about-link">Awards</a>
+					<a href="#" class="about-link">Exhibitions</a>
+					<a href="#" class="about-link">Contact</a>
+				</div>
+			</div>
+
 		<?php
 
 		while ( have_posts() ) :
